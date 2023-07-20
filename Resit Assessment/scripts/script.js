e335387a637877b65272a0e0f61df09143a902e4
nfgkh203 <117100628+nfgkh203@users.noreply.github.com>
2023-07-20 21:44:07 +0100

    Add files via upload

--- Resit Assessment/scripts/script.js
+++ Resit Assessment/scripts/script.js
@@ -1,25 +1,37 @@
+// Handle registration form submission
 document.getElementById("registerForm").addEventListener("submit", function(event){
-    event.preventDefault();    // Prevent form submission
+    // Prevent form submission to avoid page refresh
+    event.preventDefault();    
+
+    // Get input values from form
     let email = document.getElementById("registerEmail").value;
     let password = document.getElementById("registerPassword").value;
 
     // Perform some basic validation
     if(email == "" || password == "") {
+        // Notify the user to fill in all fields
         alert("Please enter an email and a password.");
     } else {
+        // Confirm successful registration
         alert("Registration successful!");
     }
 });
 
+// Handle login form submission
 document.getElementById("loginForm").addEventListener("submit", function(event){
-    event.preventDefault();    // Prevent form submission
+    // Prevent form submission to avoid page refresh
+    event.preventDefault();    
+
+    // Get input values from form
     let email = document.getElementById("loginEmail").value;
     let password = document.getElementById("loginPassword").value;
 
     // Perform some basic validation
     if(email == "" || password == "") {
+        // Notify the user to fill in all fields
         alert("Please enter an email and a password.");
     } else {
+        // Confirm successful login
         alert("Login successful!");
     }
 });
